28cc5747038bdddc3171db2503e8e19e11f586e9
Christian Moura <christianmsbrito@gmail.com>
2020-03-19 23:40:59 -0300

    fix comments service

--- src/services/comment.service.js
+++ src/services/comment.service.js
@@ -1,25 +1,25 @@
 const mongoose = require('mongoose')
-const { User } = require('../models/comment.model');
+const { Comment } = require('../models/comment.model');
 
 class Controller {
     async create(payload) {
-        return User.create(payload);
+        return Comment.create(payload);
     }
 
     async update(id, payload) {
-        return User.updateOne(mongoose.mongo.ObjectId(id), payload);
+        return Comment.updateOne(mongoose.mongo.ObjectId(id), payload);
     }
 
     async list(condition) {
-        return  User.find(condition);
+        return  Comment.find(condition);
     }
 
     async getById(id) {        
-        return User.findOne({ _id: mongoose.Types.ObjectId(id) });
+        return Comment.findOne({ _id: mongoose.Types.ObjectId(id) });
     }
 
     async remove(id) {
-        return User.delete({ _id: mongoose.mongo.ObjectId(id) });
+        return Comment.delete({ _id: mongoose.mongo.ObjectId(id) });
     }
 }
 
